d5658b2e0a7ec03e61171600b1ffa43686d395e1
Tammy Nguyen <tammynguyen@berkeley.edu>
2016-11-14 00:13:04 -0800

    remove solution

--- todo.py
+++ todo.py
@@ -18,52 +18,27 @@ app.config.update(dict(
 def index():
     return render_template('index.html')
 
+# Handle '/add' request. Fill in this function.
 @app.route('/add', methods=['POST'])
 def create():
     try:
         description = request.json['description']
-        task = Task(description, False, 1)
-        db.session.add(task)
-        db.session.commit()
+        "*** YOUR CODE HERE ***"
     except:
         return Response(response="db error", status=500)
 
     return Response(response='ok', status=200)
 
-@app.route('/list', methods=['GET'])
-def list():
-    try:
-        items = [{ 'id':          task.id,
-                   'description': task.description,
-                   'completed':   task.completed } for task in Task.query.all()]
-    except:
-        return Response(response="db error", status=500)
+# Handle '/list' request.
+# Add a function here.
 
-    return jsonify(items=items)
+# Handle '/update' request.
+# Add a function here.
 
-@app.route('/update', methods=['POST'])
-def update():
-    try:
-        info = request.json
-        task = Task.query.filter_by(id=info['id']).first()
-        task.completed = info['status']
-        db.session.commit()
-    except:
-        return Response(response="db error", status=500)
+# Handle '/delete' request.
+# Add a function here.
 
-    return Response(response="ok", status=200)
-
-@app.route('/delete', methods=['DELETE'])
-def delete():
-    try:
-        task = Task.query.filter_by(id=request.json['id']).first()
-        db.session.delete(task)
-        db.session.commit()
-    except:
-        return Response(response='db error', status=500)
-    
-    return Response(response='ok', status=200)
-
+# Run the application. 
 if __name__ == "__main__":
     # "python app.py restart" to delete db
     restart = False
